baef7b5ae6e95e819770c3f061776dc785982f8f
Mavazova <avazovam386@gmail.com>
2025-06-08 17:02:38 -0500

    Practice 4.2

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 // For these exercises, you need to declare a single variable
 // in each function that uses the appropriate collection type
 // (Array, ArrayList, HashSet, HashMap)
@@ -8,29 +11,20 @@ package org.example;
 // If you get stuck, refer to the java basics 4 (arrays and collections) curriculum
 public class CollectionChoiceExercises {
     public void MakeColorCollection() {
-        // EXAMPLE:
-        // Create an appropriate variable for holding a collection
-        // of three strings that are known ahead of time to be "Red", "Green" and "Blue"
-        // and will not change during program execution.
-
-        String[] colors;
+        String[] colors = {"Red", "Green", "Blue"};
     }
 
     public void MakeStudentCollection() {
-        // Create an appropriate collection variable for holding a list of student names
-        // that will be added to and removed from during program execution.
+        ArrayList<String> studentNames = new ArrayList<>();
     }
 
     public void MakeLoggedInUsersCollection() {
-        // Create an appropriate collection variable for holding a set of
-        // unique logged in usernames where adding a username that's already
-        // present is ignored and checking if a user is in the collection can
-        // be done very quickly.
+        HashSet<String> loggedInUsers = new HashSet<>();
+
     }
 
     public void MakeEmployeeAccessCodesCollection() {
-        // Create an appropriate collection variable for holding a set of
-        // key-value pairs where they key is an employee name (String) and the
-        // value is an employee access code (String.)
+        // Map of employee names to access codes (key-value pairs)
+        HashMap<String, String> employeeAccessCodes = new HashMap<>();
     }
 }
